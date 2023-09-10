1ea12c3e54ce2841655131a18eaea78ec6bf50c6
Vaishnav Sherla <vaishnavsherla@gmail.com>
2023-09-10 10:42:17 +0530

    Laying Foundation Of React

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const container = React.createElement('div', {}, [React.createElement('h1', {}, 'Heading 1 From Parcel'), React.createElement('h2', {}, 'Heading 2')]);
+// JSX -> React.createElement -> Obj -> HTML(DOM)
+// JSX Expression
+// Fragment > Only one parent for component
+// React Element
+const header = <h1 key="h1">Namaste</h1>
+
+// React Component (Normal js Func > React.createlment > Obj> HtmlDom)
+const HeaderComponent = () => {
+    return <h1 key="h1">Namaste Component</h1>
+}
+
+const HeaderComponent2 = () => (
+    <>
+      {header}
+      {HeaderComponent()}
+      <h1 key="h1">Namaste Component</h1>
+    </>
+  );
+
+//   const HeaderComponent2 = () => {
+//     return (
+//       <>
+//         {header}
+//         {HeaderComponent()}
+//         <h1 key="h1">Namaste Component</h1>
+//       </>
+//     );
+//   }
+  
+  
 
 // Create a React root in the 'root' div element
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Render the 'heading' component into the 'root' element
-root.render(container);
+// Render the 'header' component into the 'root' element
+// root.render(container);
+// Rendering header component u can call using normal <hc/> or hc() as its a normal func
+root.render(<HeaderComponent2/>);
